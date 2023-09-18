d956bc275463963d4d643a83863db185be5ab828
zt@ragnarok-arch <ztcjoe93@gmail.com>
2023-09-18 21:43:45 +0800

    feat(cache): cache implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/ztcjoe93/ucd
 
 go 1.21.1
+
+require github.com/jedib0t/go-pretty v4.3.0+incompatible
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/go-openapi/errors v0.20.3 // indirect
+	github.com/go-openapi/strfmt v0.21.7 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	go.mongodb.org/mongo-driver v1.11.3 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+	"time"
+
+	"github.com/jedib0t/go-pretty/table"
 )
 
 var (
 	repeatFlag int
+	listFlag   bool
+	cachePath  string
+	cacheFile  *os.File
 )
+
+type PathRecords struct {
+	Records map[string]PathRecord `json:"paths"`
+}
+
+type PathRecord struct {
+	Count     int    `json:"count"`
+	Timestamp string `json:"ts"`
+}
 
 func main() {
 	log.Printf("ucd-v0.1\n")
+
+	// flags
+	flag.BoolVar(&listFlag, "l", false, "MRU list for recently used cd commands")
+	flag.IntVar(&repeatFlag, "r", 1, "repeat dynamic cd path (for ..)")
+	flag.Parse()
+
+	args := flag.Args()
+	log.Printf("args: %v\n", args)
+	log.Printf("listFlag: %v\n", listFlag)
 
 	homeDir, _ := os.UserHomeDir()
 	curDir, _ := os.Getwd()
@@ -21,23 +47,71 @@ func main() {
 	log.Printf("~: %v\n", homeDir)
 	log.Printf("cwd: %v\n", curDir)
 
-	// flags
-	flag.IntVar(&repeatFlag, "r", 1, "repeat dynamic cd path (for ..)")
-	flag.Parse()
+	cachePath = homeDir + "/.ucd-cache"
 
-	args := flag.Args()
-	log.Printf("args: %v\n", args)
+	// cacheFile, _ = os.OpenFile(cachePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	log.Printf("Configuring cachePath to %v\n", cachePath)
+
+	cacheFile, _ := os.Open(cachePath)
+	byteValue, _ := ioutil.ReadAll(cacheFile)
+
+	var pr PathRecords
+	err := json.Unmarshal(byteValue, &pr)
+	if err != nil {
+		pr = PathRecords{Records: map[string]PathRecord{}}
+	}
+
+	// exit earlier depending on flag passed in
+	if listFlag {
+		listRecords(pr)
+		// dummy data for the time being
+		fmt.Print(".")
+
+		os.Exit(1)
+	}
 
 	if len(args) > 1 {
 		log.Fatalln("Only < 1 arguments can be passed to ucd")
 	}
 
 	// fmt.Print sends output to stdout, this will be consumed by builtin `cd` command
+
+	var targetPath string
 	if len(args) > 0 {
-		fmt.Print(repeat(args[0], repeatFlag))
+		targetPath = repeat(args[0], repeatFlag)
 	} else {
-		fmt.Print(homeDir)
+		targetPath = homeDir
 	}
+
+	rec, ok := pr.Records[targetPath]
+	if ok {
+		rec.Count++
+		rec.Timestamp = timeNow()
+		pr.Records[targetPath] = rec
+	} else {
+		pr.Records[targetPath] = PathRecord{Count: 1, Timestamp: timeNow()}
+	}
+
+	fmt.Print(targetPath)
+
+	output, _ := json.Marshal(pr)
+	ioutil.WriteFile(cachePath, output, 0644)
+	cacheFile.Close()
+}
+
+func listRecords(pr PathRecords) {
+	t := table.NewWriter()
+	t.SetOutputMirror(log.Writer())
+	t.AppendHeader(table.Row{"#", "path", "count", "timestamp"})
+
+	index := 1
+	// TODO: sort via timestamp before displaying
+	for path, rec := range pr.Records {
+		t.AppendRow([]interface{}{index, path, rec.Count, rec.Timestamp})
+		index++
+	}
+
+	t.Render()
 }
 
 func repeat(str string, times int) string {
@@ -47,4 +121,8 @@ func repeat(str string, times int) string {
 	}
 
 	return strings.Join(s, "/")
+}
+
+func timeNow() string {
+	return time.Now().Format("2006-01-02 15:04:05 MST")
 }
